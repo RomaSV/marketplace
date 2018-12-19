12336561e63c19b4ddb81992d62e643571b0e8a8
RomaSV <sve4nikov.roma@yandex.ru>
2018-12-19 16:44:04 +0300

    fix cors

--- src/main/java/com/springingdream/marketplace/config/SecurityConfig.java
+++ src/main/java/com/springingdream/marketplace/config/SecurityConfig.java
@@ -12,7 +12,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
+        http
+                .cors().and()
+                .authorizeRequests()
                 .anyRequest().permitAll()
                 .and().csrf().disable();
     }
